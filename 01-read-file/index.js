6e3c223775a94155199b810aa7d704c93f4b089d
Podexus23 <santon23@gmail.com>
2024-01-21 01:37:01 +0300

    feat: 02 is done

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,3 +5,4 @@ const process = require('process');
 const fileToRead = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(fileToRead, 'utf-8');
 stream.pipe(process.stdout);
+stream.close();
---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const readline = require('readline');
+
+const fileToWrite = path.join(__dirname, 'text.txt');
+const writable = fs.createWriteStream(fileToWrite, 'utf-8');
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('Please, write something and it will be added to the file\n');
+rl.prompt();
+rl.on('line', (line) => {
+  if (line === 'exit') {
+    process.stdout.write('Bye bye!');
+    rl.close();
+  } else {
+    if (!line.endsWith('\n')) line = `${line}\n`;
+    writable.write(line, (err) => err);
+  }
+});
+
+rl.on('SIGINT', () => {
+  process.stdout.write('Bye bye!');
+  rl.close();
+  // rl.question('Exit (y or n)? ', (input) => {
+  //   if (input.match(/^y(es)?$/i)) {
+  //     rl.pause();
+  //   }
+  // });
+});
