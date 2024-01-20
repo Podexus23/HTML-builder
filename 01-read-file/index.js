e3f22dc1e9fbd02ac57d3e3d65c3e20c61040caf
Podexus23 <santon23@gmail.com>
2024-01-20 20:16:20 +0300

    feat: 01 is done

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+const fileToRead = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(fileToRead, 'utf-8');
+stream.pipe(process.stdout);
