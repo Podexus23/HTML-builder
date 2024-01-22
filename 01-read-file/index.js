8e0246713c5c75f029a9a3b3a197c6941ee0c7e1
Podexus23 <santon23@gmail.com>
2024-01-22 15:13:31 +0300

    feat: fix 1 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,4 +5,3 @@ const process = require('process');
 const fileToRead = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(fileToRead, 'utf-8');
 stream.pipe(process.stdout);
-stream.close();
